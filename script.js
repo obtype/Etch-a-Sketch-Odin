fa15ee37c9bbf6b27d61e75478c9428a22659eda
obtype <odonotme@protonmail.com>
2023-03-08 11:13:30 +0500

    Add paint color and grid size changer
    Both features have been added and are working as intended,
    however the gui looks a bit too basic. Work needs to be done
    on that to improve it.

--- script.js
+++ script.js
@@ -7,15 +7,9 @@
 
 function createGrid(size){
     const canvas = document.querySelector('.grid');
-    let row = document.createElement('div');
-    row.classList.add('row');
-    const pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    //canvas.appendChild(pixel);
     
-
     for(let i = 0; i < size; i++){
-        row = document.createElement('div');
+        let row = document.createElement('div');
         row.classList.add('row');
         canvas.appendChild(row);
     }
@@ -27,7 +21,7 @@ function createGrid(size){
         for(let j = 0; j < size; j++){
             let pixel = document.createElement('div');
             pixel.classList.add('pixel');
-            //pixel.addEventListener('mousedown', console.log);
+            pixel.addEventListener('mousedown', changeColor);
             pixel.addEventListener('mouseenter', changeColor);
             rowlist[i].appendChild(pixel);
         }
@@ -42,13 +36,52 @@ function createGrid(size){
 
 function changeColor(e){
     if(e.buttons === 1){
-        e.target.setAttribute('style', 'background-color: black;');
-        console.log(123);
+        e.target.setAttribute('style', `background-color: ${paintColor};`);
     }
 }
 
-let paintColor = '#ff0000';
+function clearGrid(){
+    let canvas = document.querySelector('.grid');
+    let rowlist = document.querySelectorAll('.row');
+    rowlist.forEach((item) => {canvas.removeChild(item)} );
+}
 
-createGrid(10);
+function resetGrid(){
+    clearGrid();
+    createGrid(gridSize);
+}
 
-//create
+function pickInk(e){
+    paintColor = e.target.value;
+}
+
+function updateGridSizeValue(){
+    gridSizeValue.textContent = `Grid size: ${slider.value}`;
+}
+
+function changeGridSize(){
+    gridSize = slider.value;
+    resetGrid();
+}
+
+
+const resetButton = document.querySelector('.reset');
+const colorPicker = document.querySelector('#color-picker');
+const slider = document.querySelector('.slider');
+const gridSizeValue = document.querySelector('.slider-value');
+const applyGridSizeButton = document.querySelector('.apply-size');
+let paintColor = colorPicker.value;
+let gridSize = 10;
+
+
+gridSizeValue.textContent = `Grid size: ${slider.value}`;
+createGrid(gridSize);
+resetButton.addEventListener('click',resetGrid);
+colorPicker.addEventListener('change', pickInk);
+slider.addEventListener('input', updateGridSizeValue);
+applyGridSizeButton.addEventListener('click', changeGridSize);
+
+
+
+
+
